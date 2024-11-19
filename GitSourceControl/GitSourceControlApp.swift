515be8e7da14d6afd05f09d11b6178ccfb60df34
Akash <akashsingh.4111@gmail.com>
2024-11-20 00:28:38 +0530

    Initial Commit

---
+++ GitSourceControl/GitSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSourceControlApp.swift
+//  GitSourceControl
+//
+//  Created by Akash  on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GitSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
